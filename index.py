f06fcd56ff216807fab399b6f50ce6cf5856cc7c
onedrako <onedrako@gmail.com>
2022-11-08 18:35:04 -0600

    Clase 14. Variables

--- index.py
+++ index.py
@@ -2,5 +2,17 @@
 if 5 < 3:
   print("5 es mayor a 3")
 
-if 5 > 3: # Aca va otro comentario
-  print("5 es mayor a 3")
+# if 5 > 3: # Aca va otro comentario
+#   print("5 es mayor a 3")
+
+x = 5
+y = "chanchito feliz"
+
+print(x, y)
+
+correo = "chanchito@feliz.com"
+
+print(correo)
+
+_mi_var = "canchito"
+MIVAR = "canchito"
