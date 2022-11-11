ce0f286481de2e3760e5d93dcb5c3d753b915b6c
onedrako <onedrako@gmail.com>
2022-11-10 18:24:32 -0600

    Clase 31. if y condiciones

---
+++ controlflujo.py
@@ -0,0 +1,33 @@
+if 2 < 5:
+    print("2 es menor que 5")
+
+# a == b
+# a < v
+# a > b
+# a != b
+# a <= b
+# a >= b
+
+if 2 == 2:
+    print("2 es igual a 2")
+
+if 2 == 3:
+    print("2 es igual a 3")
+
+if 2 > 5:
+    print("2 es mayor que 5")
+
+if 5 > 2:
+    print("2 es menor que 5")
+
+if 2 != 2:
+    print("2 es diferente de 2")
+
+if 3 != 2:
+    print("3 es diferente de 2")
+
+if 3 >= 2:
+    print("3 es mayor o igual a 2")
+
+if 2 <= 2:
+    print("2 es mayor o igual a 2")
