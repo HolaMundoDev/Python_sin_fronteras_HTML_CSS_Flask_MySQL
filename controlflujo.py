e30a6ac413d27cfe77a392af1748f9c5d8ae7271
onedrako <onedrako@gmail.com>
2022-11-10 18:56:43 -0600

    Clase 32. if, elif y else

--- controlflujo.py
+++ controlflujo.py
@@ -1,5 +1,5 @@
-if 2 < 5:
-    print("2 es menor que 5")
+# if 2 < 5:
+#     print("2 es menor que 5")
 
 # a == b
 # a < v
@@ -8,26 +8,43 @@ if 2 < 5:
 # a <= b
 # a >= b
 
-if 2 == 2:
-    print("2 es igual a 2")
+# if 2 == 2:
+#     print("2 es igual a 2")
 
-if 2 == 3:
-    print("2 es igual a 3")
+# if 2 == 3:
+#     print("2 es igual a 3")
+
+# if 2 > 5:
+#     print("2 es mayor que 5")
+
+# if 5 > 2:
+#     print("2 es menor que 5")
+
+# if 2 != 2:
+#     print("2 es diferente de 2")
+
+# if 3 != 2:
+#     print("3 es diferente de 2")
+
+# if 3 >= 2:
+#     print("3 es mayor o igual a 2")
+
+# if 2 <= 2:
+#     print("2 es mayor o igual a 2")
 
 if 2 > 5:
-    print("2 es mayor que 5")
+    print("dos es mayor a 5 en if")
+elif 2 < 5:
+    print("2 menor a 5 en elif")
+elif 2 < 5:
+    print("2 menor a 5 en segundo elif")
+else:
+    print("yo solo me imprimo si lo anterior evalúa en falso")
 
-if 5 > 2:
-    print("2 es menor que 5")
+if 2 > 5:
+    print("dos es mayor a 5 en if")
+else:
+    print("yo solo me imprimo si lo anterior evalúa en falso")
 
-if 2 != 2:
-    print("2 es diferente de 2")
 
-if 3 != 2:
-    print("3 es diferente de 2")
 
-if 3 >= 2:
-    print("3 es mayor o igual a 2")
-
-if 2 <= 2:
-    print("2 es mayor o igual a 2")
