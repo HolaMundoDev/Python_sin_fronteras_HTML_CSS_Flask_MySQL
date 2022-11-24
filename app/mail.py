b7595065459a0f672225e9295ea9dcdf888dcc9c
onedrako <onedrako@gmail.com>
2022-11-23 22:36:13 -0600

    Clase 235. Listando Correos

--- app/mail.py
+++ app/mail.py
@@ -2,8 +2,14 @@ from flask import (
     Blueprint, render_template
     )
 
+from app.db import get_db
+
 bp = Blueprint('mail', __name__, url_prefix='/')
 @bp.route("/", methods=('GET', 'POST'))
 def index():
-    return render_template('mail/index.html')
+    db, c = get_db()
+    mails = c.execute('SELECT * FROM email')
+    mails = c.fetchall()
+
+    return render_template('mail/index.html', mails=mails)
 
