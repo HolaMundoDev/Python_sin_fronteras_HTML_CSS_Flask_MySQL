d0cf14fc77019c162d0a4a1258f60540475edcf8
onedrako <onedrako@gmail.com>
2023-07-24 13:57:18 -0600

    Clase 35. Input y primer juego

---
+++ ejercicios.py
@@ -0,0 +1,8 @@
+dato = input("ingrese dato:")
+
+lista = ["hola", "mundo", "chanchito", "feliz", "dragones"]
+
+if lista.count(dato) > 0:
+    print("el dato esta en la lista")
+else:
+    print("el dato no esta en la lista :(", dato)
