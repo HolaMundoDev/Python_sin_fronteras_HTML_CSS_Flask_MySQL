ed7cd0b7a8aede86aaab9bbb7a873b02e1350546
onedrako <onedrako@gmail.com>
2023-07-24 14:48:04 -0600

    Clase 36. Calculadora que suma

--- ejercicios.py
+++ ejercicios.py
@@ -1,8 +1,27 @@
-dato = input("ingrese dato:")
+# dato = input("ingrese dato:")
 
-lista = ["hola", "mundo", "chanchito", "feliz", "dragones"]
+# lista = ["hola", "mundo", "chanchito", "feliz", "dragones"]
 
-if lista.count(dato) > 0:
-    print("el dato esta en la lista")
+# if lista.count(dato) > 0:
+#     print("el dato esta en la lista")
+# else:
+#     print("el dato no esta en la lista :(", dato)
+
+primero = input("ingrese primer número: ")
+
+try:
+    primero = int(primero)
+except:
+    primero = "chanchito feliz"
+
+segundo = input("ingrese el segundo dato: ")
+
+try:
+    segundo = int(segundo)
+except:
+    segundo = "chanchito feliz"
+
+if primero == "chanchito feliz" or segundo == "chanchito feliz":
+    print("Ingresaste mal un dato, prueba de nuevo solo con números")
 else:
-    print("el dato no esta en la lista :(", dato)
+    print(primero + segundo)
